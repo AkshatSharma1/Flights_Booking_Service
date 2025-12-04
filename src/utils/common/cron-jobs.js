c72cac516c103407edc34fc3c89abce92f8f522a
Akshat Ajay <akshat.ajay0931@gmail.com>
2025-12-04 22:18:50 +0530

    Changed loop type

--- src/utils/common/cron-jobs.js
+++ src/utils/common/cron-jobs.js
@@ -9,31 +9,35 @@ function scheduleCrons(){
 
     //for every 30 mins(testing: use 1 minute)
     cron.schedule('*/30 * * * *', async()=>{
-        console.log("Running Cron Job: checking for expired bookings...");
+      console.log("Running Cron Job: checking for expired bookings...");
 
-        //Find: Booking solder than time: here 5 minutes
-        const timeLimit = new Date(Date.now() - 5*60*1000);
+      //Find: Booking solder than time: here 5 minutes
+      const timeLimit = new Date(Date.now() - 5 * 60 * 1000);
 
-        const expiredBookings = await bookingRepository.findExpiredBookings(timeLimit);
+      const expiredBookings = await bookingRepository.findExpiredBookings(
+        timeLimit
+      );
 
-        //if we donot get any
-        if(expiredBookings.length == 0){
-            console.log('No expired bookings');
-            return;
+      //if we donot get any
+      if (expiredBookings.length == 0) {
+        console.log("No expired bookings");
+        return;
+      }
+
+      console.log(
+        `Found ${expiredBookings.length} expired bookings. Cancelling them...`
+      );
+
+      // Process cancellation for each
+      for (const booking of expiredBookings) {
+        try {
+          await BookingService.cancelBooking(booking.id);
+          console.log(`-- Cancelled Booking ID: ${booking.id}`);
+        } catch (error) {
+          console.error(`-- Failed to cancel Booking ID: ${booking.id}`, error);
+          // throw new AppError(`-- Failed to cancel Booking ID: ${booking.id}. Error: ${error}`,StatusCodes.INTERNAL_SERVER_ERROR )
         }
-
-        console.log(`Found ${expiredBookings.length} expired bookings. Cancelling them...`);
-
-        // Process cancellation for each
-        expiredBookings.forEach(async (booking) => {
-            try {
-                await BookingService.cancelBooking(booking.id);
-                console.log(`-- Cancelled Booking ID: ${booking.id}`);
-            } catch(error) {
-                console.error(`-- Failed to cancel Booking ID: ${booking.id}`, error);
-                // throw new AppError(`-- Failed to cancel Booking ID: ${booking.id}. Error: ${error}`,StatusCodes.INTERNAL_SERVER_ERROR )
-            }
-        });
+      }
     })
 }
 
